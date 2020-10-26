a7f21491ad5cb93c6b370e7143f04ab7d71b00b1
Leonardo DaVchezt <davchezt@gmail.com>
2020-10-26 19:18:21 +0700

    Add Video

--- api.js
+++ api.js
@@ -1,7 +1,40 @@
 ﻿const express = require('express');
 const router = express.Router();
 
+var hmacSHA256 = require('crypto-js/hmac-sha256');
+var Base64 = require('crypto-js/enc-base64');
+
+function getSignature(data) {
+    var API_KEY = 'QUjeYXypMZoyEZHezSon65NiGJW2ZVoPVq7AJ5Pm6QU';
+    var api_key = API_KEY;
+    var json_data = Buffer.from(JSON.stringify(data)).toString('base64');
+    var hash_hmac = Base64.stringify(hmacSHA256(json_data, api_key)); //already base64 encoded
+    var sign_data = `${api_key}.${hash_hmac}`;
+    var result = Buffer.from(sign_data).toString('base64').replace('=', '');
+
+    return result;
+}
+
 router.get('/', (req, res, next) => {
+    var mainObj = {
+        id: 1,
+        value: "static"
+    };
+    
+    var mainData = {
+        action:"AGENDA",
+        description:"Sample description about the action",
+        platform:"ANGULAR",
+        data: JSON.stringify(mainObj)
+    };
+    
+    var xhrData = {
+        data: mainData,
+        signature: getSignature(JSON.stringify(mainObj))
+    };
+
+    console.log(getSignature(JSON.stringify(mainObj)));
+
     res.status(200).json({
         "status": 200,
         "message": "siap digunakan",
